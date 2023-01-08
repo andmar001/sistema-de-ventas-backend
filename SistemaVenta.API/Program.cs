ecd4101e97f8cbe6cc3cd27e7c52baabb3a7d561
tony tolver <andrademaresantonio00@gmail.com>
2023-01-07 23:29:05 -0600

    Configurar servicio de cadena de conexion

--- SistemaVenta.API/Program.cs
+++ SistemaVenta.API/Program.cs
@@ -1,3 +1,5 @@
+using SistemaVenta.IOC; //conexion db
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// metodo de extensión con cadena de conexión
+builder.Services.InyectarDependencias(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ SistemaVenta.IOC/Dependencia.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaVenta.DAL.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenta.IOC
+{
+    public static class Dependencia
+    {
+        //Recibir un servicio de conexiones - metodo de extension
+        public static void InyectarDependencias( this IServiceCollection service, IConfiguration configuration)
+        {
+            //agregar contexto de nuestra referencia de la cadena de conexión 
+            service.AddDbContext<DbventaContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("cadenaSQL"));
+            });
+        }
+    }
+}
