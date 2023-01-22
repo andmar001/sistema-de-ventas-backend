8c00b5ccdac104d92ef1c6890896438b411f8e18
tony tolver <andrademaresantonio00@gmail.com>
2023-01-21 21:35:21 -0600

    Capa BLL - Logica de servicios

---
+++ SistemaVenta.BLL/Servicios/DashBoardService.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using AutoMapper;
+using SistemaVenta.Model;
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.DTO;
+using System.Globalization;
+
+namespace SistemaVenta.BLL.Servicios
+{
+    public class DashBoardService : IDashBoardService
+    {
+        private readonly IVentaRepository _ventaRepositorio;
+        private readonly IGenericRepository<Producto> _productoRepositorio;
+        private readonly IMapper _mapper;
+
+        public DashBoardService(IVentaRepository ventaRepositorio, IGenericRepository<Producto> productoRepositorio, IMapper mapper)
+        {
+            _ventaRepositorio = ventaRepositorio;
+            _productoRepositorio = productoRepositorio;
+            _mapper = mapper;
+        }
+        
+        private IQueryable<Venta> RetornarVentas(IQueryable<Venta> tablaVenta, int restarCantidadDias) 
+        {
+            //obtener la ultima fecha de la venta
+            DateTime? ultimaFecha = tablaVenta.OrderByDescending(v => v.FechaRegistro).Select(v => v.FechaRegistro).First();
+
+            ultimaFecha = ultimaFecha.Value.AddDays(restarCantidadDias);
+
+            return tablaVenta.Where(v => v.FechaRegistro.Value.Date >= ultimaFecha.Value.Date);
+        }
+
+        private async Task<int> TotalVentasUltimaSemana()
+        {
+            var total = 0;
+            IQueryable<Venta> _ventaQuery = await _ventaRepositorio.Consultar();
+
+            if (_ventaQuery.Count() > 0)
+            {
+                //ventas de hace 7 días
+                var tablaVenta = RetornarVentas(_ventaQuery, -7);
+                total = tablaVenta.Count();
+            }
+            return total;
+        }
+
+        private async Task<string> TotalIngresosUltimaSemana()
+        {
+            decimal resultado = 0;
+            IQueryable<Venta> _ventaQuery = await _ventaRepositorio.Consultar();
+
+            if (_ventaQuery.Count() > 0)
+            {
+                var tablaVenta = RetornarVentas(_ventaQuery, -7);
+
+                resultado = tablaVenta.Select(v => v.Total).Sum(v => v.Value);
+            }
+
+            return Convert.ToString(resultado, new CultureInfo("es-PE"));
+        }
+
+        private async Task<int> TotalProductos()
+        {
+            IQueryable<Producto> _productoQuery = await _productoRepositorio.Consultar();
+
+            int total = _productoQuery.Count();
+
+            return total;
+        }
+
+        private async Task<Dictionary<string,int>> VentasUltimaSemana()
+        {
+            Dictionary<string, int> resultado = new Dictionary<string, int>();
+
+            IQueryable<Venta> _ventaQuery = await _ventaRepositorio.Consultar();
+
+            if (_ventaQuery.Count() > 0)
+            {
+                var tablaVenta = RetornarVentas(_ventaQuery, -7);
+
+                resultado = tablaVenta
+                    .GroupBy(v => v.FechaRegistro.Value.Date).OrderBy(g => g.Key)
+                    .Select(dv => new { fecha = dv.Key.ToString("dd/MM/yyyy"), total = dv.Count() })
+                    .ToDictionary(keySelector: r => r.fecha, elementSelector: r => r.total);
+
+            }
+            return resultado;
+        }
+
+        public async Task<DashBoardDTO> Resumen()
+        {
+            DashBoardDTO vmDashboard = new DashBoardDTO();
+
+            try
+            {
+                vmDashboard.TotalVentas = await TotalVentasUltimaSemana();
+                vmDashboard.TotalIngresos = await TotalIngresosUltimaSemana();
+                vmDashboard.TotalProductos = await TotalProductos();
+
+                List<VentaSemanaDTO> listaVentaSemana = new List<VentaSemanaDTO>();
+
+                foreach (KeyValuePair<string, int> item in await VentasUltimaSemana())
+                {
+                    //actualizar la lista de ventas en cada iteracion
+                    listaVentaSemana.Add(new VentaSemanaDTO()
+                    {
+                        Fecha = item.Key,
+                        Total = item.Value,
+                    });
+                }
+                vmDashboard.VentasUltimaSemana = listaVentaSemana;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return vmDashboard;
+        }
+    }
+}
--- SistemaVenta.IOC/Dependencia.cs
+++ SistemaVenta.IOC/Dependencia.cs
@@ -11,12 +11,16 @@ using SistemaVenta.DAL.DBContext;
 using SistemaVenta.DAL.Repositorios.Contrato;
 using SistemaVenta.DAL.Repositorios;
 
+using SistemaVenta.Utility;
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.BLL.Servicios;
+
 namespace SistemaVenta.IOC
 {
     public static class Dependencia
     {
         //Recibir un servicio de conexiones - metodo de extension
-        public static void InyectarDependencias( this IServiceCollection service, IConfiguration configuration)
+        public static void InyectarDependencias(this IServiceCollection service, IConfiguration configuration)
         {
             //agregar contexto de nuestra referencia de la cadena de conexión 
             service.AddDbContext<DbventaContext>(options =>
@@ -25,7 +29,19 @@ namespace SistemaVenta.IOC
             });
 
             service.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));   //inyeccion de dependencia de manera generica, uso en cualquier modelo
-            service.AddScoped<IVentaRepository,VentaRepository>();                             //inyeccion de dependencia . solo modelo de venta
+            service.AddScoped<IVentaRepository, VentaRepository>();                             //inyeccion de dependencia . solo modelo de venta
+
+            //automapper
+            service.AddAutoMapper(typeof(AutoMapperProfile));
+
+            //Dependencias de servicios BLL
+            service.AddScoped<IRolService, RolService>();
+            service.AddScoped<IUsuarioService, UsuarioService>();
+            service.AddScoped<ICategoriaService, CategoriaService>();
+            service.AddScoped<IProductosService, ProductoService>();
+            service.AddScoped<IVentaService, VentaService>();
+            service.AddScoped<IDashBoardService, DashBoardService>();
+            service.AddScoped<IMenuService, MenuService>();
         }
     }
 }
